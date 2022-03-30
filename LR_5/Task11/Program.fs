f68ed1f933b2da29614aea21db1d74cacd0ab907
Sultan <sultangordovg@gmail.com>
2022-03-31 00:14:10 +0300

    5.11 READY
    Сделан первый номер из 5-ой лабы

---
+++ LR_5/Task11/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let answer n =
+    match n with
+    | "F#"|"Prolog" -> "Подлиза"
+    | "c++" -> "based"
+    | "c#" -> "based++"
+    | "assembler" -> "ладно"
+    |_ -> "ну ты выдал!"
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Любимый ЯП?")
+    Console.WriteLine(answer (Console.ReadLine()))
+    0
