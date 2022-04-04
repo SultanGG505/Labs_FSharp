48936459d5118df6e417df7ca37a6c8defd30385
Sultan <sultangordovg@gmail.com>
2022-04-04 17:19:22 +0300

    5.12 READY

---
+++ LR_5/Task12/Task12.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+let answer n =
+    match n with
+    | "F#"|"Prolog" -> "Подлиза"
+    | "c++" -> "based"
+    | "c#" -> "based++"
+    | "assembler" -> "ладно"
+    |_ -> "ну ты выдал!"
+
+[<EntryPoint>]
+let main argv =
+   System.Console.WriteLine("Какой ваш любимый язык программирования?")
+   //Случай суперпозиции
+   (Console.ReadLine>>answer>>Console.WriteLine)()
+
+   //----------------------------------------------------------------------//
+
+   System.Console.WriteLine("Какой ваш любимый язык программирования?")
+  // Случай каррирования
+   let F y x z =z(x(y()))
+   F Console.ReadLine answer Console.WriteLine
+   0 
