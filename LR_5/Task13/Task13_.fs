38b49a2507e9c11ebd867c14547fa50f88d8013b
Sultan <sultangordovg@gmail.com>
2022-04-04 19:03:02 +0300

    5.13 ready

---
+++ LR_5/Task13/Task13_.fs
@@ -0,0 +1,51 @@
+﻿open System
+//Произведение цифр с помощью рекурсии вверх
+let rec pr_up x =
+    match x with
+    |x when x<>1->(x%10)*(pr_up(x/10))
+    |x->1
+ 
+ //поиск цифры с максимальным значением с помощью рекурсии вверх
+let rec max_up x =
+    match x with
+    |x when x<10-> x
+    |x->max (x % 10) (max_up (x/10))
+ 
+//поиск цифры с минимальным значением с помощью рекурсии вверх
+let rec min_up x =
+    match x with
+    |x when x<10-> x
+    |x->min (x % 10) (min_up (x/10))
+ 
+//Произведение цифр с помощью рекурсии вниз
+let rec pr_down x a =
+    match x with 
+    | x when x=0 ->a
+    | x->pr_down(x/10) a*(x%10)
+ 
+//поиск максимальной цифры с помощью рекурсии вниз
+let rec max_down x a =
+     match x with 
+    |x when (x%10)>a->max_down(x/10)(x%10)
+    |x when x=0 ->a
+    |x->max_down(x/10)a
+ 
+//поиск минимальной цифры с помощью рекурсии вниз
+let rec min_down x a =
+     match x with 
+    | x when (x<>0)&&((x%10)<a)->min_down(x/10)(x%10)
+    | x when x=0 ->a
+    | x->min_down(x/10)a
+ 
+ 
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите число:")
+    let x = System.Convert.ToInt32(System.Console.ReadLine())
+    Console.WriteLine("Произведение рекурсией вверх:{0}", pr_up x)
+    Console.WriteLine("Максимум:{0}", max_up x)
+    Console.WriteLine("Минимум:{0}", min_up x)
+    Console.WriteLine("Произведение:{0}", pr_down x 1)
+    Console.WriteLine("Максимум:{0}", max_down x 0)
+    Console.WriteLine("Минимум:{0}", min_down x 9)
+    0 
