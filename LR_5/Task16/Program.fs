626d446844fc9c376ab56ceb90083fae11c8c6e1
Sultan <sultangordovg@gmail.com>
2022-04-04 21:10:19 +0300

    5.16 READY

--- LR_5/Task16/Program.fs
+++ LR_5/Task16/Program.fs
@@ -6,7 +6,7 @@ let rec NOD a b =
         if a>b then NOD (a-b) b 
         else NOD a (b-a) 
 
-(*let Eiler n func init =
+let Eiler n func init =
     let rec Eiler_ n func init a=
         if a<=0 then init
           else 
@@ -14,7 +14,7 @@ let rec NOD a b =
           let new_a=a-1
           Eiler_ n func newInit new_a
     Eiler_ n func init n
- *)
+ 
 let GO x F init =
     let rec GO_ x F init number =
         if number = 0 then init
@@ -30,6 +30,6 @@ let main argv =
     System.Console.WriteLine("Введите число:")
     let x = System.Convert.ToInt32(System.Console.ReadLine())
     System.Console.WriteLine("Сумма:{0}", GO x (fun x y -> x+y) 0)
-    (*System.Console.WriteLine("Функция Эйлера от числа:{0}", Eiler x (fun x  -> x + 1) 0)*)
+    System.Console.WriteLine("Функция Эйлера от числа:{0}", Eiler x (fun x  -> x + 1) 0) //36->12
     0 
- 
+    
