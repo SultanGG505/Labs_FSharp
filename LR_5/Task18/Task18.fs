ec54953583dfabbf1d7ffcb55ffb20db8062ea06
Sultan <sultangordovg@gmail.com>
2022-04-05 01:40:55 +0300

    5.18 method 2

--- LR_5/Task18/Task18.fs
+++ LR_5/Task18/Task18.fs
@@ -18,10 +18,21 @@ let MaxDel a init =
            let newCand=cand-1
            max_ a newInit newCand
     max_ a init a
+//произведение цифр числа, не делящихся на 5
+let proizved a init=
+    let rec pr a init =
+        if a=0 then init else
+           let newInit = if (a%10)%5<>0 then init*(a%10) else init
+           let newCand=a/10
+           pr newCand newInit 
+    pr a init 
 
 [<EntryPoint>]
 let main argv =
     Console.WriteLine("Введите число")
     let a=System.Convert.ToInt32(System.Console.ReadLine())
     Console.WriteLine("Максимальный простой делитель числа:{0}",MaxDel a 0)
-    0 
+    Console.WriteLine("Произведение цифр числа, не кратных 5:{0}",proizved a 1)
+    
+    
+    0 // return an integer exit code
