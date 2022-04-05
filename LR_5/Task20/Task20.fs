fb08c6470306dd8e9af1399b6e8a3c8b30279786
Sultan <sultangordovg@gmail.com>
2022-04-05 11:12:52 +0300

    5.20 READY

--- LR_5/Task20/Task20.fs
+++ LR_5/Task20/Task20.fs
@@ -11,21 +11,21 @@ let check_simple a=
              simple a newcand
     simple a (a-1)
 
-let max_simple_del a init =
+let MAX_SIMPLE_DEL a  =
     let rec max_ a init cand=
         if cand<=0 then init else
            let newInit= if check_simple cand && a%cand=0 && cand>init then cand else init
            let newCand=cand-1
            max_ a newInit newCand
-    max_ a init a
+    max_ a 1 a
 //произведение цифр числа, не делящихся на 5
-let PR_NO_MOD_5 a init=
-    let rec pr_ a init =
+let PR_NO_MOD_5 a =
+    let rec PR_ a init =
         if a=0 then init else
            let newInit = if (a%10)%5<>0 then init*(a%10) else init
            let newCand=a/10
-           pr_ newCand newInit 
-    pr_ a init 
+           PR_ newCand newInit 
+    PR_ a 1 
 //Найти НОД максимального нечетного непростого делителя
 //числа и прозведения цифр данного числа.
 let rec NOD n m=
@@ -35,29 +35,33 @@ let rec NOD n m=
     let newm=if n<=m then m%n else m
     NOD newn newm
 
-let PR a init=
-    let rec PR_ a init=
+let PR a =
+    let rec P_ a init=
         if a=0 then init else
            let newinit=init*(a%10)
            let newa=a/10
-           PR_ newa newinit
-    PR_ a init
+           P_ newa newinit
+    P_ a 1
 
-let MAX_Nechet_Simple a init=
-    let rec max1 a init cand=
+let MAX_Nechet_Simple a =
+    let rec MAX_ a init cand=
         if cand=0 then init else
            let newInit=if a%cand=0 && cand%2=1 && not(check_simple a) && cand>init then cand else init
            let newCand=cand-1
-           max1 a newInit newCand
-    max1 a init a
+           MAX_ a newInit newCand
+    MAX_ a 0 a
+
+let SBEU_NOD a = NOD (PR a) (MAX_Nechet_Simple a)
+
+let select =function
+    | 1-> MAX_SIMPLE_DEL
+    | 2-> PR_NO_MOD_5
+    | 3-> SBEU_NOD
+    | _-> SBEU_NOD
+    
 [<EntryPoint>]
 let main argv =
-    Console.WriteLine("Введите число")
-    let number =Convert.ToInt32(System.Console.ReadLine())
-    Console.WriteLine("Максимальный простой делитель числа:{0}",max_simple_del number 0)
-    Console.WriteLine("Произведение цифр числа, не кратных 5:{0}",PR_NO_MOD_5 number 1)
-    Console.WriteLine("(1)Произведение цифр числа:{0}",PR number 1)
-    Console.WriteLine("(2)Максимальный нечетный непростой делитель:{0}",MAX_Nechet_Simple number 1)
-    Console.WriteLine("НОД (1) и (2):{0}",NOD (PR number 1) (MAX_Nechet_Simple number 1) )
-    
+    let a = (Console.ReadLine() |> Int32.Parse, Console.ReadLine() |> Int32.Parse)
+    let result = select (fst a) (snd a)
+    System.Console.WriteLine(result)
     0 // return an integer exit code
