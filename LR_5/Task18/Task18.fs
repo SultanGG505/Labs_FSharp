35bf7393698a7d628b9321597c4fb33ee4cddb2f
Sultan <sultangordovg@gmail.com>
2022-04-05 09:27:00 +0300

    5.18 READY(WITH METHOD 3)

--- LR_5/Task18/Task18.fs
+++ LR_5/Task18/Task18.fs
@@ -26,13 +26,37 @@ let proizved a init=
            let newCand=a/10
            pr newCand newInit 
     pr a init 
+//Найти НОД максимального нечетного непростого делителя
+//числа и прозведения цифр данного числа.
+let rec NOD a b =
+    if a=b then a
+    else 
+        if a>b then NOD (a-b) b 
+        else NOD a (b-a) 
 
+let pr a init=
+    let rec p a init=
+        if a=0 then init else
+           let newinit=init*(a%10)
+           let newa=a/10
+           p newa newinit
+    p a init
+
+let max a init=
+    let rec max1 a init cand=
+        if cand=0 then init else
+           let newInit=if a%cand=0 && cand%2=1 && not(ober_simple a) && cand>init then cand else init
+           let newCand=cand-1
+           max1 a newInit newCand
+    max1 a init a
 [<EntryPoint>]
 let main argv =
     Console.WriteLine("Введите число")
     let a=System.Convert.ToInt32(System.Console.ReadLine())
     Console.WriteLine("Максимальный простой делитель числа:{0}",MaxDel a 0)
     Console.WriteLine("Произведение цифр числа, не кратных 5:{0}",proizved a 1)
-    
+    Console.WriteLine("(1)Произведение цифр числа:{0}",pr a 1)
+    Console.WriteLine("(2)Максимальный нечетный непростой делитель:{0}",max a 1)
+    Console.WriteLine("НОД (1) и (2):{0}",NOD (pr a 1) (max a 1) )
     
     0 // return an integer exit code
