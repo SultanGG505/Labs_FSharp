49102b58a32a45ada1e0293f1976a5faca4d0048
Sultan <sultangordovg@gmail.com>
2022-04-06 20:47:39 +0300

    LR 5 already done (fixed finally 13)
    Были переписаны 3 функции в 13-ой задаче, больше недочётов в 5-ой лабе нет, лаба закрыта.

--- LR_5/Task13/Task13_.fs
+++ LR_5/Task13/Task13_.fs
@@ -18,7 +18,7 @@ let rec min_up x =
     |x when x<10-> x
     |x->min (x % 10) (min_up (x/10))
  
-//Произведение цифр с помощью рекурсии вниз
+//Произведение цифр с помощью рекурсии вниз(+ хвостовая рекурсия)
 let rec pr_down x a =
     match x with 
     | x when x=0 ->a
@@ -29,17 +29,21 @@ let rec pr_down x a =
  
 //поиск максимальной цифры с помощью рекурсии вниз
 let rec max_down x a =
+     let x_1 = x % 10
+     let x_2 = x / 10
      match x with 
-    |x when (x%10)>a->max_down(x/10)(x%10)
+    |x when (x_1)>a->max_down(x_2)(x_1)
     |x when x=0 ->a
-    |x->max_down(x/10)a
+    |x->max_down(x_2)a
  
 //поиск минимальной цифры с помощью рекурсии вниз
 let rec min_down x a =
+     let x_1 = x % 10
+     let x_2 = x / 10
      match x with 
-    | x when (x<>0)&&((x%10)<a)->min_down(x/10)(x%10)
+    | x when (x<>0)&&((x_1)<a)->min_down(x_2)(x_1)
     | x when x=0 ->a
-    | x->min_down(x/10)a
+    | x->min_down(x_2)a
  
  
 [<EntryPoint>]
