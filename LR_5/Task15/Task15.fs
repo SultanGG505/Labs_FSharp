8e9edd499c61ec716abf94ed49863e741a5319c7
Sultan <sultangordovg@gmail.com>
2022-04-07 19:50:02 +0300

    6.11 READY

--- LR_5/Task15/Task15.fs
+++ LR_5/Task15/Task15.fs
@@ -6,15 +6,7 @@ let rec NOD a b =
         if a>b then NOD (a-b) b 
         else NOD a (b-a) 
 
-(*let Eiler n func init =
-    let rec Eiler_ n func init a=
-        if a<=0 then init
-          else 
-          let newInit=if NOD n a=1 then func init else init 
-          let new_a=a-1
-          Eiler_ n func newInit new_a
-    Eiler_ n func init n
- *)
+
 let GO x F init =
     let rec GO_ x F init number =
         if number = 0 then init
--- LR_6/Task11/Task11.fs
+++ LR_6/Task11/Task11.fs
@@ -1,1 +1,38 @@
-﻿
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = Convert.ToInt32(Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n= Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+let rec writeList = function
+    [] ->   let z = Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       Console.WriteLine(head)
+                       writeList tail  
+
+let rec change list (func: int -> int -> int -> int) =
+    match list with 
+    | a::b::c::t -> (func a b c) :: (change t func)
+    | a::b::[] -> (func a b 1) :: (change [] func)
+    | a::[] -> (func a 1 1) :: (change [] func)
+    | [] -> []
+
+
+[<EntryPoint>]
+let main argv =
+    (* Console.WriteLine("Ввод n")
+    let n = Convert.ToInt32(Console.ReadLine()) *)
+    let list = readData
+    Console.WriteLine("text")
+    change list (fun x y z -> x+y+z) |> writeList
+    0
