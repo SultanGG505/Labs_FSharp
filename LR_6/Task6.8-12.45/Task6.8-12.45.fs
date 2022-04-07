3f730c9d2be5609c873d26c383b3a3e007ad84e7
Sultan <sultangordovg@gmail.com>
2022-04-08 01:03:58 +0300

    6.12.45 READY

---
+++ LR_6/Task6.8-12.45/Task6.8-12.45.fs
@@ -0,0 +1,43 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = Convert.ToInt32(Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n= Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+let rec writeList = function
+    [] ->   let z = Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       Console.WriteLine(head)
+                       writeList tail  
+
+let Adder (a,b) list init= 
+    let rec A_ list (a,b) sum = 
+        match list with
+        |[]->sum
+        |h::tail-> 
+            let newSum = 
+                if h>a && h<b then sum+h
+                else sum
+            A_ tail (a,b) newSum
+    A_ list (a,b) 0
+[<EntryPoint>]
+let main argv =
+    printfn "Введите количество элементов списка:"
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    printfn "Введите элементы списка:"
+    let list = readList n
+    Console.WriteLine("Введите границы интервала")
+    let interval = (Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()))
+    printfn "Сумма элементов:"
+    Console.WriteLine(Adder interval list 0)
+    0
