1a0ff1b0c478964ac36a90447c192d33596a27ea
Sultan <sultangordovg@gmail.com>
2022-04-07 21:30:01 +0300

    6.12.13(3) READY

---
+++ LR_6/Task_6.4-12.13/Task_6.4-12.13.fs
@@ -0,0 +1,47 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = Convert.ToInt32(Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n= Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+let rec writeList = function
+    [] ->   let z = Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       Console.WriteLine(head)
+                       writeList tail  
+
+let min_el list =
+    let rec m_ list init=
+        match list with
+        |[]->init
+        |h::t ->
+          let newInit =if h<init then h else init
+          let newList=t
+          m_ newList newInit          
+    m_ list list.Head
+
+let change list=
+    let rec c_ list =
+        match list with
+        |[]->list
+        |h::t -> 
+           if min_el list = h then list
+           else 
+           let newList=t@[h]
+           c_ newList
+    c_ list
+
+        
+[<EntryPoint>]
+let main argv =
+    readData|> change|>writeList 
