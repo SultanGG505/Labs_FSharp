cc3d0944b0e4df0718280ea72a5132167bc74769
Sultan <sultangordovg@gmail.com>
2022-04-09 00:38:57 +0300

    Task 7.19/2 READY

--- LR_7/7.19/Task_7.19.fs
+++ LR_7/7.19/Task_7.19.fs
@@ -3,6 +3,7 @@
 (*
 3 Дана строка в которой слова записаны через пробел. Необходимо
 перемешать все слова этой строки в случайном порядке.
+8 Дана строка в которой записаны слова через пробел. Необходимо посчитать количество слов с четным количеством символов.
 *)
 
 let rec writeString = function
@@ -53,10 +54,29 @@ let ToString (list: 'string list) =
             let newS = str + " " + h
             r tail newS
     r list ""
+//
+let rec Acc list (f: string->int->int) p acc = 
+    match list with 
+    |[]->acc
+    | h::tail->
+            if p h then
+              let newAcc= f h acc
+              Acc tail f p newAcc
+              else Acc tail f p acc
+
+let check_count (s:string) = 
+    let s1= drop s
+    let res = Acc s1 (fun x y-> if (x.Length % 2 = 0) then y + 1 else y ) (fun x-> true) 0
+    res
+
+    
 
 [<EntryPoint>]
 let main argv =
     Console.WriteLine "Введите строку, которую хотите перемешать"
     let (s:string) = Console.ReadLine()
     Console.WriteLine (ToString(RND s))
+
+    Console.WriteLine "Количество слов с чётным количеством букв"
+    Console.WriteLine (check_count s)
     0
