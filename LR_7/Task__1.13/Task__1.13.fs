c3ca61ff9465c26bc857c6b8d6659d57f292cde4
Sultan <sultangordovg@gmail.com>
2022-04-08 19:46:37 +0300

    7__1.13 READY

---
+++ LR_7/Task__1.13/Task__1.13.fs
@@ -0,0 +1,31 @@
+﻿open System
+
+(*
+Дан целочисленный массив. Необходимо разместить элементы,
+расположенные до минимального, в конце массива.
+*)
+
+let rec readList n =
+    Console.WriteLine("Введите элементы:")
+    List.init(n) (fun index->Console.ReadLine()|>Int32.Parse)
+
+let readData = 
+    Console.WriteLine("Введите количество элементов:")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
+let rec writelist list=
+    List.iter(fun x->printfn "%O" x) list
+
+
+[<EntryPoint>]
+let main argv =
+    let list = readData
+    let min = List.min list
+    let ind= List.findIndex (fun x -> x=min) list
+    let index = List.indexed list
+    let List1= list.[0..(ind-1)]
+    let List2=list.[ind..List.length list]
+    Console.WriteLine("answer - ")
+    writelist(List2@List1)
+    0 // return an integer exit code
