5f3dbca540456ea787d6c7b62d788e588893c57b
Sultan <sultangordovg@gmail.com>
2022-04-08 19:29:01 +0300

    7__1.3 READY

--- LR_7/Task__1.3/Task__1.3.fs
+++ LR_7/Task__1.3/Task__1.3.fs
@@ -1,1 +1,30 @@
-﻿
+﻿open System
+
+(*
+Дан целочисленный массив и натуральный индекс (число, меньшее
+размера массива). Необходимо определить является ли элемент по
+указанному индексу глобальным максимумом
+*)
+
+let rec readList n =
+    Console.WriteLine("Введите элементы:")
+    List.init(n) (fun index->Console.ReadLine()|>Int32.Parse)
+
+let readData = 
+    Console.WriteLine("Введите количество элементов:")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
+let rec writelist list=
+    List.iter(fun x->printfn "%O" x) list
+
+let Check list n =
+    if List.item n list = List.max list then true else false
+
+[<EntryPoint>]
+let main argv =
+    let list = readData
+    Console.WriteLine("Введите индекс:")
+    let num = Convert.ToInt32(Console.ReadLine())
+    Check list num|>Console.WriteLine
+    0 // return an integer exit code
