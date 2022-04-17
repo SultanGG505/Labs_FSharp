7d80e77209710bf8a0c30cce6b695ade93bd2344
Sultan <sultangordovg@gmail.com>
2022-04-17 12:20:38 +0300

    Task5 and Task6 READY
    задача 6 заключается в выводе документа на экран, сделал её сразу в 5-ой задаче

---
+++ LR_8/Task_5/Task_5-6.fs
@@ -0,0 +1,20 @@
+﻿// Создать класс, содержащий информацию о правах на вождение
+open System
+
+type License(i: int, LN: string, FN: string, P: string, bD: string, eAD: string, Cat: string) = 
+    let mutable id = i
+    let mutable LastName = LN
+    let mutable FirstName = FN
+    let mutable Patronymic = P
+    let mutable birthDay = bD
+    let mutable endActiveDay = eAD
+    let mutable category = Cat
+    
+    override this.ToString() = 
+        $"Права номер:{id}\nФИО: {LastName} {FirstName} {Patronymic}\nДата рождения: {birthDay}\nКатегория: {category}\nДата окончания действительности прав: {endActiveDay}"
+
+[<EntryPoint>]
+let main argv =
+    let license = License(1433,"Гордов","Султан","Николаевич","01.12.2002","01.12.2043","A")
+    Console.WriteLine(license.ToString())
+    0 
