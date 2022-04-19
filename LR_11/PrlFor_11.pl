1a9ff35227209d93315b180bb1da44b1786eef03
Sultan <sultangordovg@gmail.com>
2022-04-19 14:34:02 +0300

    LR_11 Task11 READY
    Оказывается была проблема в том, что я создал базу данных пролог с именами с больших букв, это вызывало ошибку, которую я уже починил.
    И соответственно, задача 11 решена.

--- LR_11/PrlFor_11.pl
+++ LR_11/PrlFor_11.pl
@@ -1,87 +1,75 @@
-man(Artem).
-man(Egor).
-man(Daniil).
-man(Sultan).
-man(Valentin).
-man(Alexey).
-man(Artur).
-man(Kirill).
-man(Ivan).
-man(Anton).
-man(Andrei).
-man(Arseniy).
-man(Valera).
+man(artem).
+man(egor).
+man(daniil).
+man(sultan).
+man(valentin).
+man(alexey).
+man(artur).
+man(kirill).
+man(ivan).
+man(anton).
+man(andrei).
+man(arseniy).
+man(valera).
 
-woman(Olga).
-woman(Kristina).
-woman(Katya).
-woman(Valentina).
-woman(Svetlana).
-woman(Rita).
-woman(Eleonora).
-woman(Polina).
-woman(Tatyana).
-woman(Anna).
-woman(Nastya).
-woman(Natasha).
-woman(Veronika).
+woman(olga).
+woman(kristina).
+woman(katya).
+woman(valentina).
+woman(svetlana).
+woman(rita).
+woman(eleonora).
+woman(polina).
+woman(tatyana).
+woman(anna).
+woman(nastya).
+woman(natasha).
+woman(veronika).
 
-parent(Artem,Egor).
-parent(Artem,Daniil).
-parent(Artem,Sultan).
-parent(Artem,Valentin).
+parent(artem,egor).
+parent(artem,daniil).
+parent(artem,sultan).
+parent(artem,valentin).
 
-parent(Kristina,Egor).
-parent(Kristina,Daniil).
-parent(Kristina,Sultan).
-parent(Kristina,Valentin).
+parent(kristina,egor).
+parent(kristina,daniil).
+parent(kristina,sultan).
+parent(kristina,valentin).
 
-parent(Alexey,Kirill).
-parent(Alexey,Tatyana).
-parent(Eleonora,Kirill).
-parent(Eleonora,Tatyana).
+parent(alexey,kirill).
+parent(alexey,tatyana).
+parent(eleonora,kirill).
+parent(eleonora,tatyana).
 
-parent(Ivan,Anton).
-parent(Ivan,Polina).
-parent(Anna,Anton).
-parent(Anna,Polina).
+parent(ivan,anton).
+parent(ivan,polina).
+parent(anna,anton).
+parent(anna,polina).
 
-parent(Andrei,Rita).
-parent(Andrei,Svetlana).
-parent(Katya,Rita).
-parent(Katya,Svetlana).
+parent(andrei,rita).
+parent(andrei,svetlana).
+parent(katya,rita).
+parent(katya,svetlana).
 
-parent(Arseniy,Nastya).
-parent(Arseniy,Valera).
-parent(Natasha,Nastya).
-parent(Natasha,Valera).
-
-parent(X,Y,Z):- parent(X,Y),parent(Z,Y),man(X),woman(Z),print(Y),nl,fail.
+parent(arseniy,nastya).
+parent(arseniy,valera).
+parent(natasha,nastya).
+parent(natasha,valera).
 
 man:-man(X),print(X),nl,fail.
 woman:- woman(X),print(X),nl,fail.
-parent:- parent(X,zdislava),print(X),nl,fail.
-mother(X,Y):-woman(X),parent(X,Y).
-father(X,Y):-man(X),parent(X,Y).
-brother(X,Y):- parent(Z,X),parent(Z,Y),X\=Y.
-grand_pa(X,Y):-parent(X,Z),parent(Z,Y).
-uncle(X,Y):-brother(X,Z),parent(Z,Y).
-max(X,Y,Z):-(X>=Y -> Z is X);(Y>=X -> Z is Y).
-fact_up(0,1).
-fact_up(N,X):-N1 is N-1,fact_up(N1,X1),X is X1*N.
-fact_down(0,X,X).
-fact_down(N,A,X):-A1 is A*N,N1 is N-1,fact_down(N1,A1,X).
-fact_down(N,X):-fact_down(N,1,X).
-sum_dig(0,0).
-sum_dig(N,X):-N1 is N div 10,sum_dig(N1,X1),X is X1+(N mod 10).
-
-
 
 /* Task 11*/
 /*Вариант № 3. Построить предикат daughter(X, Y), который проверяет,
 является ли X дочерью Y. Построить предикат, daughter(X), который
 выводит дочь X.*/
 
-
+daughter(X):- parent(X,Y),woman(Y),write(Y),nl,fail.
 daughter(X,Y):- woman(X),parent(Y,X).
+
+
+
+
+
+
 
