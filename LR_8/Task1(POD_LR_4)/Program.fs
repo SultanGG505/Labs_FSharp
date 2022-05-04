7a6ecef98b17c225ee1c26fca021055e5f2dff67
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-05-04 12:19:45 +0300

    LR_8 task1 IPrint ready

--- LR_8/Task1(POD_LR_4)/Program.fs
+++ LR_8/Task1(POD_LR_4)/Program.fs
@@ -1,5 +1,9 @@
 ﻿open System
 
+type IPrint = interface
+    abstract member Print: unit -> unit
+    end
+
 [<AbstractClass>]
 type Shape() = 
     abstract member square: unit -> double
@@ -11,13 +15,19 @@ type Rectangle(w: double, h: double) =
     override this.square() = width * height
     override this.ToString() = 
         $"Прямоугольник с шириной {width}, высотой {height} и площадью {width*height}"
-    
+    interface IPrint with
+           member this.Print(): unit = printfn "%s"(this.ToString())
+           end
+
 type Square(s: double) =
     inherit Rectangle(s,s)
     let mutable side = s
     override this.square() = side * side
     override this.ToString() = 
            $"Квадрат со стороной {side} и площадью {side*side}"
+    interface IPrint with
+           member this.Print(): unit = printfn "%s"(this.ToString())
+           end
 
 type Circle(r: double) =
     inherit Shape()
@@ -25,3 +35,19 @@ type Circle(r: double) =
     override this.square() = Math.PI * r * r
     override this.ToString() = 
         $"Окружность с радиусом {radius} и площадью {Math.PI * radius * radius}"
+    interface IPrint with
+           member this.Print(): unit = printfn "%s"(this.ToString())
+           end
+
+[<EntryPoint>]
+let main argv =
+    let rect = Rectangle(10.0,20.0)
+    (rect :> IPrint).Print()
+
+    let squr = Square(10.0)
+    (squr :> IPrint).Print()
+
+    let circ = Circle(10.0)
+    (circ :> IPrint).Print()
+    
+    0
