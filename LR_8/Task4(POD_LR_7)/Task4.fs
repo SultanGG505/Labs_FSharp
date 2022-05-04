a926f42a4761f1b47a2c821c9ae6d8369383d8f0
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-05-05 00:24:47 +0300

    LR_8 Task4 READY/ LR_8 READY

---
+++ LR_8/Task4(POD_LR_7)/Task4.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+let printerAgent = MailboxProcessor.Start(fun inbox->
+    let rec messageLoop() = async{
+        let! msg = inbox.Receive()        
+        match msg with
+        | "F#"|"Prolog" -> Console.WriteLine("Подлиза") 
+        | "c++" -> Console.WriteLine("based") 
+        | "c#" -> Console.WriteLine("based++") 
+        | "assembler" -> Console.WriteLine("ладно") 
+        |_ -> Console.WriteLine("ну ты выдал!") 
+        return! messageLoop()
+        }
+    messageLoop()
+ )
+[<EntryPoint>]
+let main argv =
+    let arg1 = printerAgent.Post("c++")
+    let arg2 = printerAgent.Post("assembler")
+    let arg2 = printerAgent.Post("Prolog")    
+    Console.ReadKey()
+    0 
