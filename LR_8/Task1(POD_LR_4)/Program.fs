da726e52b16ba37a359fe84fb0abd44bce5896f9
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-05-04 11:39:30 +0300

    LR_8 task1 toString ready

--- LR_8/Task1(POD_LR_4)/Program.fs
+++ LR_8/Task1(POD_LR_4)/Program.fs
@@ -9,14 +9,19 @@ type Rectangle(w: double, h: double) =
     let mutable width = w
     let mutable height = h
     override this.square() = width * height
+    override this.ToString() = 
+        $"Прямоугольник с шириной {width}, высотой {height} и площадью {width*height}"
     
 type Square(s: double) =
     inherit Rectangle(s,s)
     let mutable side = s
     override this.square() = side * side
+    override this.ToString() = 
+           $"Квадрат со стороной {side} и площадью {side*side}"
 
 type Circle(r: double) =
     inherit Shape()
     let mutable radius = r
     override this.square() = Math.PI * r * r
-    
+    override this.ToString() = 
+        $"Окружность с радиусом {radius} и площадью {Math.PI * radius * radius}"
