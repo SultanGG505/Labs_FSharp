333d5f444d2c0c7c18fa7fc35376329edb7fead2
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-05-04 11:31:34 +0300

    LR_8 task 1 created main classes
    Создал основные классы фигур

---
+++ LR_8/Task1(POD_LR_4)/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+[<AbstractClass>]
+type Shape() = 
+    abstract member square: unit -> double
+
+type Rectangle(w: double, h: double) = 
+    inherit Shape()
+    let mutable width = w
+    let mutable height = h
+    override this.square() = width * height
+    
+type Square(s: double) =
+    inherit Rectangle(s,s)
+    let mutable side = s
+    override this.square() = side * side
+
+type Circle(r: double) =
+    inherit Shape()
+    let mutable radius = r
+    override this.square() = Math.PI * r * r
+    
