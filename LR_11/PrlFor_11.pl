77afc51b3297b806085813e27427400f55675e6c
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-05-05 21:17:27 +0300

    changed /* */ to %

--- LR_11/PrlFor_11.pl
+++ LR_11/PrlFor_11.pl
@@ -59,27 +59,20 @@ parent(natasha,valera).
 man:-man(X),print(X),nl,fail.
 woman:- woman(X),print(X),nl,fail.
 
-/* Task 11*/
-/*Вариант № 3. Построить предикат daughter(X, Y), который проверяет, является ли  X дочерью Y.
- Построить предикат, daughter(X), который выводит дочь X. 
- */
+% Task 11 Вариант № 3. Построить предикат daughter(X, Y), который проверяет, является ли  X дочерью Y.
+% Построить предикат, daughter(X), который выводит дочь X. 
 
 daughter(X,Y):- woman(X),parent(Y,X).
 daughter(X):- parent(X,Y),woman(Y),write(Y),nl,fail.
 
-/*Task 12*/
-/*Вариант № 3. Построить предикат wife(X, Y), который проверяет, является ли X женой Y.
- Построить предикат wife(X), который выводит жену X.
- */
+% Task 12 Вариант № 3. Построить предикат wife(X, Y), который проверяет, является ли X женой Y.
+% Построить предикат wife(X), который выводит жену X.
 
 wife(X,Y):- parent(X,Z),parent(Y,Z), ((woman(X);woman(Y)),(man(X);man(Y))).
 wife(X):- man(X),parent(X,Z),parent(Y,Z),woman(Y),write(Y).
 
-/*Task 13*/
-/*
-Вариант 3. Построить предикат grand_ma(X, Y), который проверяет, является ли X бабушкой Y. 
-Построить предикат grand_mas(X), который выводит всех бабушек X.
-*/
+% Task 13 Вариант 3. Построить предикат grand_ma(X, Y), который проверяет, является ли X бабушкой Y. 
+% Построить предикат grand_mas(X), который выводит всех бабушек X.
 
 man(valera1).
 man(valera2).
@@ -91,22 +84,17 @@ woman(anna3).
 
 parent(anna1,anna2).
 parent(anna2,anna3).
-/*anna1- ������� anna3(видимо кракозябра про то, кто кому бабушка)*/
+% anna1- ������� anna3(видимо кракозябра про то, кто кому бабушка)
 
 grand_ma(X,Y):- woman(X),parent(X,Z),parent(Z,Y).
 grand_mas(X) :- parent(Y,X),parent(Z,Y),print(Z),nl,fail.
 
-/*Task 14*/
-/*Вариант 3. Построить предикат grand_ma_and_da(X,Y), 
-который проверяет, являются ли X и Y бабушкой и внучкой или внучкой и бабушкой.
-*/
+% Task 14 Вариант 3. Построить предикат grand_ma_and_da(X,Y), 
+% который проверяет, являются ли X и Y бабушкой и внучкой или внучкой и бабушкой.
 
 grand_ma_and_da(X,Y) :- woman(Y),woman(X),(grand_ma(X,Y);grand_ma(Y,X)).
 
-/*Task 15*/
-/*
-Найти минимальную цифру числа с помощью рекурсии вверх.
-*/
+% Task 15 Найти минимальную цифру числа с помощью рекурсии вверх.
 
 mindigit_up(0,9) :- !.
 mindigit_up(X,Digit) :- X1 is X div 10, mindigit_up(X1,Dig1), Dig2 is X mod 10, (Dig1 < Dig2, Digit is Dig1; Digit is Dig2), !.
