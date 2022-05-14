caa8301b6b322de22ca563afaa9162649d1b606f
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-05-14 08:46:30 +0300

    LR_8 FINALLY READY with time comparisment
    бинарный поиск самый быстрый, уху

--- LR_8/Task_5/Task_5-6-7-8.fs
+++ LR_8/Task_5/Task_5-6-7-8.fs
@@ -1,6 +1,8 @@
 ﻿// Создать класс, содержащий информацию о правах на вождение
 open System.Text.RegularExpressions
 open System
+open System.Diagnostics
+
 
 type License(i_n: int,i_s: int, LN: string, FN: string, P: string, bD: string, eAD: string, Cat: string) = 
     member this.id_num 
@@ -44,7 +46,7 @@ type License(i_n: int,i_s: int, LN: string, FN: string, P: string, bD: string, e
     member this.birthDay
         with get() = bD
         and set(newValue: string) =
-            if (Regex.IsMatch (newValue, @"[0-9]{2}.[0-9]{2}.[0-9]{4}"))
+            if (Regex.IsMatch (newValue.ToString(), @"[0-9]{2}.[0-9]{2}.[0-9]{4}"))
             then this.birthDay <- newValue
             else failwith "error"
 
@@ -74,18 +76,92 @@ type License(i_n: int,i_s: int, LN: string, FN: string, P: string, bD: string, e
             license.id_num = this.id_num && license.id_ser = this.id_ser // проверка по номеру и серии прав
         | _ -> false
 
+    interface IComparable with
+        member this.CompareTo obj =
+            match obj with
+            | :? License as Lc -> this.id_num.CompareTo(Lc.id_num)
+            | _ -> 1
+        end
+    override this.GetHashCode() = this.id_num.GetHashCode()
+
+
+[<AbstractClass>]
+type DocumentCollection() =
+    abstract member searchDocument: License -> Boolean
+
+type DocumentList(l: License list) =
+    inherit DocumentCollection()
+    member this.list: (License list) = l
+    override this.searchDocument (doc: License) = 
+        (this.list |> List.filter (fun x -> x = doc)).Length > 0
+
+type DocumentArray(l: License list) =
+    inherit DocumentCollection()
+    member this.array: (License array) = Array.ofList l
+    override this.searchDocument (doc: License) = 
+        Array.exists (fun x -> x = doc) this.array
+
+type DocumentSet(l: License list) =
+    inherit DocumentCollection()
+    member this.set: (Set<License>) = Set.ofList l
+    override this.searchDocument (doc: License) = 
+        Set.contains doc this.set
+
+type DocumentBynary(l: License list) =
+    inherit DocumentCollection()
+    let rec binarySearch (l:License list) (el:License) =
+        match (List.length l) with
+        |0->false
+        |i->
+            let mid = i/2
+            match sign <| compare el l.[mid] with
+            |0->true
+            |1->binarySearch l.[..mid - 1] el
+            |_->binarySearch l.[mid + 1..] el    
+            
+    member this.list: (License list) = l |> List.sortBy (fun x -> x.id_num)
+    override this.searchDocument (doc: License) = binarySearch this.list doc
+
+
+
+
 
 [<EntryPoint>]
-let main argv =
-    let licenseF1 = License(1433,105050,"Гордов", "Султан","Николаевич","01.12.2002","01.12.2043","A")
-    let licenseF2 = License(1311,105050,(*пример работы failwith, поменять на имя*)"1319309d0a9wdju91jh0","Андрей","Егорович","12.01.2002","01.12.2022","B")
+let main argv =    
+    let random = System.Random()
+    let listOfDocs = [ for i in 1 .. 1000000 -> License(1433,105050,"Гордов", "Султан","Николаевич",random.Next(100000,999999).ToString(),"01.12.2043","A")]
     
-    Console.WriteLine(licenseF1.ToString())
-    Console.WriteLine(licenseF2.ToString())
+    let randDoc = License(1433,105050,"Гордов", "Султан","Николаевич",random.Next(100000,999999).ToString(),"01.12.2043","A")
+    let arr = DocumentArray(listOfDocs)
+    let ls = DocumentList(listOfDocs)
+    let set = DocumentSet(listOfDocs)
+    let bynary = DocumentBynary(listOfDocs)
 
-    //licenseF1.FirstName <- "1319309d0a9wdju91jh0"
+    let watch = new Stopwatch()
+    watch.Start()
+    ls.searchDocument(randDoc)
+    watch.Stop()
+    Console.WriteLine("Список: {0}", watch.Elapsed)
+    watch.Restart()
+
+    let watch = new Stopwatch()
+    watch.Start()
+    arr.searchDocument(randDoc)
+    watch.Stop()
+    Console.WriteLine("Массив: {0}", watch.Elapsed)
+    watch.Restart()
+
+    let watch = new Stopwatch()
+    watch.Start()
+    set.searchDocument(randDoc)
+    watch.Stop()
+    Console.WriteLine("Множество: {0}", watch.Elapsed)
+    watch.Restart()
     
-    printfn "%A"((licenseF1 = licenseF2))
-   
-   
-    0 
+    let watch = new Stopwatch()
+    watch.Start()
+    bynary.searchDocument(randDoc)
+    watch.Stop()
+    Console.WriteLine("Бинарный поиск: {0}", watch.Elapsed)
+    watch.Restart()
+    0
